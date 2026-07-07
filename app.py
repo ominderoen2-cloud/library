195e00269c757981a3b8938e5eb892d1b9fe4c3f
Roen Ominde <ominderoen2@gmail.com>
2026-07-07 12:07:00 +0300

    Prepare application for Render deployment

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from database.database import create_member_table , create_books_table , create_borrow_books_table , users , create_users_table
+from database.database import create_member_table , create_books_table , create_borrow_books_table , create_users_table
 from routes.routes import member_bp
-from models.user import User
 import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
@@ -8,12 +7,6 @@ app = Flask(__name__)
 app.register_blueprint(member_bp)
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 jwt = JWTManager(app)
-users.append(
-    User(
-        username="roen",
-        password="1234"
-    )
-)
 @app.route("/")
 def home():
     return {"status":"api running"}
@@ -21,7 +14,12 @@ create_member_table()
 create_borrow_books_table()
 create_books_table()
 create_users_table()
-if "__main__" == __name__:
-    app.run(host ="0.0.0.0" , port = 67 , debug = True)
+port = int(os.getenv("PORT", 67))
+
+app.run(
+    host="0.0.0.0",
+    port=port,
+    debug=True
+)
 
 #library/app.py
